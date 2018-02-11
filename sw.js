5a846721e7349e32532dcfbe09e057aa24b7dc54
Nigel Schuster <nigel.schusters@googlemail.com>
2018-02-11 14:27:50 -0500

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-78de55eca1f2f9976b9d.js","ca47518c16f2f8765d65a4f4e448f65e"],["/app-e489e37596e6e348c960.js","15f791d0c0784fb8afabc1e0187c8bfb"],["/app-fb7e8a6acb1bab2c16ea.js","4ea0b621d92458604ac44e9597665dc8"],["/commons-b434bb43fdd9acfe7660.js","d71d91fee6025641bc3f4225c4b2f00f"],["/index.html","d1d9a3cc4ee151f9646bbb1de4ef43d5"],["/manifest.json","cb2dfe86f62602b1cf5032c8cd6ade4a"],["/offline-plugin-app-shell-fallback/index.html","05030b7ac6ea26d7d85344d18283e266"]];
+var precacheConfig = [["/app-78de55eca1f2f9976b9d.js","ca47518c16f2f8765d65a4f4e448f65e"],["/app-e489e37596e6e348c960.js","15f791d0c0784fb8afabc1e0187c8bfb"],["/app-fb7e8a6acb1bab2c16ea.js","4ea0b621d92458604ac44e9597665dc8"],["/commons-b434bb43fdd9acfe7660.js","d71d91fee6025641bc3f4225c4b2f00f"],["/index.html","8613851fa4dcdaa990bd3646b59792dd"],["/manifest.json","cb2dfe86f62602b1cf5032c8cd6ade4a"],["/offline-plugin-app-shell-fallback/index.html","ca9030e8cf6a24fc6f6d74d675cda823"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
