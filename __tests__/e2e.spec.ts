28018bdf9e35e5f3ef4b6418e5c7534568516344
Nigel Schuster <nigel.schusters@googlemail.com>
2021-09-01 23:05:26 -0400

    Add wait

--- __tests__/e2e.spec.ts
+++ __tests__/e2e.spec.ts
@@ -2,26 +2,14 @@
  * @jest-environment puppeteer
  */
 const path = require("path");
+jest.setTimeout(60000);
 describe("Homepage loads", () => {
   it("works", async () => {
     await page.setUserAgent(
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
     );
-    const messages = [`--user-data-dir=${process.cwd()}`];
-    page
-      .on("console", (message) =>
-        messages.push(
-          `${message.type().substr(0, 3).toUpperCase()} ${message.text()}`,
-        ),
-      )
-      .on("pageerror", ({ message }) => messages.push(message))
-      .on("response", (response) =>
-        messages.push(`${response.status()} ${response.url()}`),
-      )
-      .on("requestfailed", (request) =>
-        messages.push(`${request.failure().errorText} ${request.url()}`),
-      );
     await page.goto("http://localhost:4444/");
+      await page.waitFor(10*1000);
     const image = await page.screenshot();
 
     expect(image).toMatchImageSnapshot();
