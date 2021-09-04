2a31dcb595a6d3ecaa2851fdca5e1a4d4954686c
Nigel Schuster <nigel.schusters@googlemail.com>
2021-09-04 16:19:15 -0400

    Change sitemap base url

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://nigel-schuster.de',
+    siteUrl: process.env.SITE_URL || 'https://www.nigel-schuster.de',
     generateRobotsTxt: true,
     changefreq: 'weekly',
     autoLastmod: false
