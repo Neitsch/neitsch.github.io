a5395e8705eb0bdfa9cc486a6fd360b64adeca34
Nigel Schuster <nigel.schusters@googlemail.com>
2021-09-11 10:08:37 -0400

    Coverage (#36)
    * Coverage
    
    * Improve source map
    
    * Remove unused
    
    * Bump @apollo/client from 3.4.10 to 3.4.11 (#37)
    
    Bumps [@apollo/client](https://github.com/apollographql/apollo-client) from 3.4.10 to 3.4.11.
    - [Release notes](https://github.com/apollographql/apollo-client/releases)
    - [Changelog](https://github.com/apollographql/apollo-client/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/apollographql/apollo-client/compare/v3.4.10...v3.4.11)
    
    ---
    updated-dependencies:
    - dependency-name: "@apollo/client"
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump sharp from 0.29.0 to 0.29.1 (#38)
    
    Bumps [sharp](https://github.com/lovell/sharp) from 0.29.0 to 0.29.1.
    - [Release notes](https://github.com/lovell/sharp/releases)
    - [Changelog](https://github.com/lovell/sharp/blob/master/docs/changelog.md)
    - [Commits](https://github.com/lovell/sharp/compare/v0.29.0...v0.29.1)
    
    ---
    updated-dependencies:
    - dependency-name: sharp
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Coverage
    
    * Improve source map
    
    * Remove unused
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ components/__tests__/date.test.tsx
@@ -0,0 +1,19 @@
+import Date from "../date";
+import { render } from "@testing-library/react";
+import React from "react";
+
+describe("Date", () => {
+  it("renders a heading", () => {
+    const { container } = render(<Date dateString="2021-02-02" />);
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <time
+          datetime="2021-02-02"
+        >
+          February	2, 2021
+        </time>
+      </div>
+    `);
+  });
+});
---
+++ jest.config.ts
@@ -0,0 +1,64 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  // automock: false,
+  // bail: 0,
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+      '**/*.{js,jsx,ts,tsx}',
+      '!**/*.d.ts',
+      '!**/node_modules/**',
+      '!**/generated/**',
+      '!*.{js,ts}',
+  ],
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: [
+      "/node_modules/",
+      "/.next/",
+      "/out/",
+      "/coverage/",
+  ],
+  coverageProvider: "babel",
+  // coverageReporters: [
+  //   "json",
+  //   "text",
+  //   "lcov",
+  //   "clover"
+  // ],
+  // coverageThreshold: undefined,
+  errorOnDeprecated: true,
+  moduleFileExtensions: [
+    "js",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
+    "node"
+  ],
+    moduleNameMapper: {
+        '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+        '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+        '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: "jsdom",
+  testMatch: [
+    "**/__tests__/**/*.test.[jt]s?(x)",
+  ],
+  testPathIgnorePatterns: [
+      '<rootDir>/node_modules/', '<rootDir>/.next/', '<rootDir>/out/'
+  ],
+    transform: {
+        /* Use babel-jest to transpile tests with the next/babel preset
+           https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
+        '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    },
+    transformIgnorePatterns: [
+        '/node_modules/',
+        '^.+\\.module\\.(css|sass|scss)$',
+    ],
+};
