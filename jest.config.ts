122395215bb0996e4a4d4faccb686ec5142c9779
Nigel Schuster <nigel.schusters@googlemail.com>
2021-09-11 12:37:35 -0400

    Mutation testing

--- jest.config.ts
+++ jest.config.ts
@@ -55,7 +55,7 @@ export default {
     transform: {
         /* Use babel-jest to transpile tests with the next/babel preset
            https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
-        '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+        '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest'],
     },
     transformIgnorePatterns: [
         '/node_modules/',
---
+++ stryker.conf.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  packageManager: "npm",
+  reporters: ["html", "clear-text", "progress"],
+  testRunner: "jest",
+  coverageAnalysis: "perTest",
+    mutate: ['{lib,components,pages}/**/*.ts?(x)', '!{lib,components,pages}/**/*@(.test|.spec|Spec).ts?(x)'],
+    // buildCommand: "babel",
+    ignorePatterns: [
+        "coverage", "out", ".next", "public", "generated", "/*.{ts,js,tsx,json}", ".github", "__mocks__", ".*"
+    ],
+    "jest": {
+        "config": {
+            "testEnvironment": "@stryker-mutator/jest-runner/jest-env/jsdom"
+        },
+    }
+};
