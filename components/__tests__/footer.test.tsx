eded16c454337de1a02b6def1dd7ffa734858e5b
Neitsch <Neitsch@users.noreply.github.com>
2022-05-14 11:21:24 +0000

    Update snapshots

--- components/__tests__/footer.test.tsx
+++ components/__tests__/footer.test.tsx
@@ -9,26 +9,26 @@ describe("footer", () => {
     const { container } = render(<Footer />);
 
     expect(container).toMatchInlineSnapshot(`
-<div>
-  <footer>
-    <div
-      class="MuiContainer-root MuiContainer-maxWidthLg css-1oqqzyl-MuiContainer-root"
-    >
-      <p
-        class="MuiTypography-root MuiTypography-body1 css-ahj2mt-MuiTypography-root"
-      >
-        Built with NextJS. Find the source 
-        <a
-          class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways css-1iwtne7-MuiTypography-root-MuiLink-root"
-          href="https://github.com/Neitsch/neitsch.github.io"
-        >
-          here
-        </a>
-        .
-      </p>
-    </div>
-  </footer>
-</div>
-`);
+      <div>
+        <footer>
+          <div
+            class="MuiContainer-root MuiContainer-maxWidthLg css-1oqqzyl-MuiContainer-root"
+          >
+            <p
+              class="MuiTypography-root MuiTypography-body1 css-ahj2mt-MuiTypography-root"
+            >
+              Built with NextJS. Find the source 
+              <a
+                class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways css-1ps4owl-MuiTypography-root-MuiLink-root"
+                href="https://github.com/Neitsch/neitsch.github.io"
+              >
+                here
+              </a>
+              .
+            </p>
+          </div>
+        </footer>
+      </div>
+    `);
   });
 });
