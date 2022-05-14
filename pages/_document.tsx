51adcb7b6f090ff421de5048ea5f69406bb01c4b
Neitsch <Neitsch@users.noreply.github.com>
2022-05-14 12:11:05 +0000

    Fix lint a bit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,11 +12,17 @@ import type {
   RenderPageResult,
 } from "next/dist/shared/lib/utils";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import React, {ReactFragment, ReactElement} from "react";
+import React from "react";
+import type { ReactFragment, ReactElement } from "react";
 
 export default class MyDocument extends Document {
   public static async getInitialProps(ctx: DocumentContext): Promise<{
-    styles: (string | number | ReactFragment | ReactElement<unknown, string | JSXElementConstructor<unknwon>>)[];
+    styles: (
+      | ReactElement<unknown, JSXElementConstructor<unknwon> | string>
+      | ReactFragment
+      | number
+      | string
+    )[];
     html: string;
     head?: (JSX.Element | null)[] | undefined;
   }> {
